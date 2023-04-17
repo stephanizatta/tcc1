16f675b7936228f5af1bdef76e07b1a21f29049a
Stephani <stephani.zatta@ambevtech.com.br>
2023-04-17 20:50:32 -0300

    register button

---
+++ src/pages/cadastro/cadastro.js
@@ -0,0 +1,10 @@
+import './cadastro.css';
+import React from 'react';
+
+function Cadastro() {
+    return (
+        <p> Teste </p>
+    );
+  }
+  
+  export default Cadastro;
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -2,11 +2,12 @@ import './login.css';
 import React from 'react';
 import { Box, Button, Paper, TextField, Typography } from '@mui/material';
 
-function Login() {
+function Login(props) {
   return (
     <Box display='flex' justifyContent='center' style={{ width: '100%' }}>
       <Paper elevation={3} sx={{ p: 3 }} style={{ width: '30rem' }}>
         <Box display='flex' justifyContent='center' flexDirection='column'>
+
           <Typography>Login</Typography>
           <TextField
             required
@@ -24,8 +25,38 @@ function Login() {
           />
 
           <Box>
-            <Button variant='text'>Esqueceu sua senha?</Button>
-            <Button color='primary' variant='contained'>Entrar</Button>
+            <Box>
+              <Button 
+                color='primary' 
+                variant='contained' 
+                style={{ width: '7rem' }}
+                onClick={() => {
+                  alert('WIP-Entrar');
+                }} 
+              >
+                Entrar
+              </Button>
+
+              <Button
+                color='primary' 
+                variant='contained' 
+                style={{ width: '7rem' }}
+                onClick={() => {
+                  alert('WIP-Cadstrar');
+                }} 
+              >
+                Cadastrar
+              </Button>
+            </Box>
+
+            <Button 
+              variant='text'
+              onClick={() => {
+                alert('WIP-Esqueceu a senha');
+              }} 
+            >
+              Esqueceu sua senha?
+            </Button>
           </Box>
         </Box>
       </Paper>
