8779edc7c884d8481d9c3c0f106747d100d3ada0
Ana Maria Maurizio <anamaurizio@hotmail.com>
2023-04-17 23:54:22 -0300

    alteracao background responsividade e layout da tela de login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,21 +1,21 @@
 import './login.css';
 import React from 'react';
-import { Box, Button, Paper, TextField, Typography } from '@mui/material';
+import { Box, Button, Checkbox, Input, Paper, TextField, Typography } from '@mui/material';
 
 function Login(props) {
   return (
     <Box display='flex' justifyContent='center' style={{ width: '100%' }}>
       <Paper elevation={3} sx={{ p: 3 }} style={{ width: '30rem' }}>
         <Box display='flex' justifyContent='center' flexDirection='column'>
-
           <Typography>Login</Typography>
           <TextField
             required
             id="outlined-required"
             label="E-mail"
+            type= "e-mail"
             placeholder="Digite seu e-mail"
           />
-
+            <br></br>
           <TextField
             required
             id="outlined-password-input"
@@ -23,40 +23,10 @@ function Login(props) {
             type="password"
             autoComplete="current-password"
           />
-
+          <br></br>
           <Box>
-            <Box>
-              <Button 
-                color='primary' 
-                variant='contained' 
-                style={{ width: '7rem' }}
-                onClick={() => {
-                  alert('WIP-Entrar');
-                }} 
-              >
-                Entrar
-              </Button>
-
-              <Button
-                color='primary' 
-                variant='contained' 
-                style={{ width: '7rem' }}
-                onClick={() => {
-                  alert('WIP-Cadstrar');
-                }} 
-              >
-                Cadastrar
-              </Button>
-            </Box>
-
-            <Button 
-              variant='text'
-              onClick={() => {
-                alert('WIP-Esqueceu a senha');
-              }} 
-            >
-              Esqueceu sua senha?
-            </Button>
+            <Button variant='text'>Esqueceu sua senha?</Button>
+            <Button color='primary' variant='contained'>Entrar</Button>
           </Box>
         </Box>
       </Paper>
