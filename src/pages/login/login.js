b32aa3d83aa8c05e9ec59eff7833c479069caf51
Stephani <stephani.zatta@ambevtech.com.br>
2023-05-23 19:52:42 -0300

    login page

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,67 +1,106 @@
 import './login.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Button, Paper, TextField, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+  
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleLogin() {
+    if (email === 'admin' && password === 'admin') {
+      // Lógica de login para o usuário 'admin'
+      // Redirecionar para a página '/home'
+      window.location.href = '/home';
+    } else {
+      // Credenciais inválidas
+      setErrorMessage('Credenciais inválidas. Por favor, verifique seu e-mail e senha.');
+    }
+  }
+
+  function handleCadastro() {
+    window.location.href = '/cadastro';
+  }
+
   return (    
     <div className='backgroundLogin'>
-    <Box display='flex' justifyContent='center' style={{ width: '100%' }}>
-      <Paper elevation={3} sx={{ p: 3 }} style={{ width: '25rem' }}>
-        <Box display='flex' justifyContent='center' flexDirection='column'>
-          <Typography variant="h4" component="h1" align="center">Login</Typography>
-          <br/>
-          <TextField
-            required
-            id="outlined-required"
-            label="E-mail"
-            placeholder="Digite seu e-mail"
-          />
-          <br/>
-          <TextField
-            required
-            id="outlined-password-input"
-            label="Senha"
-            type="password"
-            autoComplete="current-password"
+      <Box display='flex' justifyContent='center' style={{ width: '100%' }}>
+        <Paper elevation={3} sx={{ p: 3 }} style={{ width: '25rem' }}>
+          <Box display='flex' justifyContent='center' flexDirection='column'>
+            <Typography variant="h4" component="h1" align="center">Login</Typography>
 
-          />
-          <br/>
-          <Box>
+            <br/>
+
+            <TextField
+              required
+              id="outlined-required"
+              label="E-mail"
+              placeholder="Digite seu e-mail"
+              value={email}
+              onChange={handleEmailChange}
+            />
+
+            <br/>
+
+            <TextField
+              required
+              id="outlined-password-input"
+              label="Senha"
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+
+            <br/>
+
+            {errorMessage && (
+              <p style={{ color: 'red', textAlign: 'center'}}>
+                {errorMessage}
+              </p>
+            )}
 
             <Box>
-              <Link to='/home'>
+              <Box>
                 <Button
                   color='primary'
                   variant='contained'
                   style={{ width: '7rem', marginRight: '1rem' }}
+                  onClick={handleLogin}
                 >
                   Entrar
                 </Button>
-              </Link>
 
-              <Link to='/cadastro'>
                 <Button
                   color='primary'
                   variant='contained'
                   style={{ width: '7rem' }}
+                  onClick={handleCadastro}
                 >
                   Cadastrar
                 </Button>
-              </Link>
-            </Box>
+              </Box>
 
-            <Link to='esqueceu-senha'>
-              <Button
-                variant='text'
-              >
-                Esqueceu sua senha?
-              </Button>
-            </Link>
+              <Link to='esqueceu-senha'>
+                <Button
+                  variant='text'
+                >
+                  Esqueceu sua senha?
+                </Button>
+              </Link>
+
+            </Box>
           </Box>
-        </Box>
-      </Paper>
-    </Box>
+        </Paper>
+      </Box>
     </div>
   );
 }
